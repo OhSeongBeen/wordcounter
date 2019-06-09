0ac5e47cdbbefecfd9ecd60ad6a764222053f400
OhSeongBeen <44831012+OhSeongBeen@users.noreply.github.com>
2019-06-09 22:43:48 +0900

    change urlpatterns

--- firstproject/urls.py
+++ firstproject/urls.py
@@ -19,8 +19,8 @@ import wordcount.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', wordcount.views.home, name="home"),
+    path('home/', wordcount.views.home, name="home"),
     path('about/', wordcount.views.about, name="about"),
     path('result/', wordcount.views.result, name="result"),
-    path('index/', wordcount.views.index, name="index"),
+    path('', wordcount.views.index, name="index"),
 ]
