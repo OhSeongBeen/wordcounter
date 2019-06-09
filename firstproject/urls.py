4fa189ba8f8e8deb88eb280f6d09a2d8fcc66856
OhSeongBeen <44831012+OhSeongBeen@users.noreply.github.com>
2019-06-09 22:48:14 +0900

    check mis spelling

--- firstproject/urls.py
+++ firstproject/urls.py
@@ -19,8 +19,8 @@ import wordcount.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', wordcount.views.home, name="home"),
+    path('', wordcount.views.home, name="home"),
     path('about/', wordcount.views.about, name="about"),
     path('result/', wordcount.views.result, name="result"),
-    path('', wordcount.views.index, name="index"),
+    path('index/', wordcount.views.index, name="index"),
 ]
